c026ebba42f3373fa549f31c360c62ea5fe1d74f
xtx2022 <1793071674@qq.com>
2023-04-29 13:02:23 +0800

    Test Finished

---
+++ TestLab1/cmake-build-debug/CMakeFiles/TestLab1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestLab1.dir/main.cpp.o"
+  "CMakeFiles/TestLab1.dir/main.cpp.o.d"
+  "TestLab1"
+  "TestLab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestLab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TestLab1/cmake-build-debug/CMakeFiles/TestLab1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TestLab1.
+# This may be replaced when dependencies are built.
