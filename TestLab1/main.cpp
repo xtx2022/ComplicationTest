61c6427f6c49bbfe9b29ce2c58fdd1404cb06755
xtx2022 <1793071674@qq.com>
2023-04-29 14:24:42 +0800

    valgrind test passed

--- TestLab1/main.cpp
+++ TestLab1/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+// /mnt/c/users/xia/Documents/GitHub/ComplicationTest/TestLab1
+
+void fun()
+{
+    int* a;
+    a = new int[10];
+    delete[] a;
+}
+
+void fun1()
+{
+    int* a;
+    a = new int[1000];
+}
 
 int main() {
+    fun1();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
